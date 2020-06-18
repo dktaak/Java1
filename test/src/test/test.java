1d8e3ca4443e3a722388ba01b0faf55a97db6ce1
Infosys <Infosys@Dinesh>
2020-06-18 20:35:21 +0530

    change

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("this is my 1st test git project");
-		System.out.println("again test");
+		System.out.println("again");
 	}
 
 }
