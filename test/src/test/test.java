f3ad02028b964f29c957c8f4844bba5e799d359a
dktaak <dktaak23@gmail.com>
2020-06-19 01:52:21 +0530

    for(int i:a)

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -3,10 +3,23 @@ package test;
 public class test {
 
 	public static void main(String[] args) {
+		/*
 		boolean x=true;
 		boolean y=false;
 		System.out.println(x && y);
 		System.out.println(x || y);
+	*/
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=10;
+		a[2]=10;
+		a[3]=10;
+		a[4]=10;
+		for(int i:a)
+		{		
+			System.out.println(i);
+		}
+		
 	}
 
 }
