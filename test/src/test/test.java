a227928440b2117c02f80050c5ea5cf22dabc238
dktaak <dktaak23@gmail.com>
2020-06-19 02:21:31 +0530

    2d array

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -10,15 +10,17 @@ public class test {
 		System.out.println(x || y);
 	*/
 		
-		Object a[]=new Object[5];
-		a[0]= 10.25;
-		a[1]="bhbhbh";
-		a[2]='m';
-		a[3]=false;
-		a[4]=true;
-		for(Object i:a)
-		{		
-			System.out.println(i);
+		Object a[][]=new Object[3][2];
+		a[0][0]= 10.25;
+		a[0][1]="bhbhbh";
+		a[1][0]='m';
+		a[1][1]=false;
+		a[2][0]=true;
+		for(int i=0;i<3;i++)
+		{		for(int j=0;j<2;j++)
+		{
+			System.out.println(a[i][j]+"  ");
+		}
 		}
 		
 	}
